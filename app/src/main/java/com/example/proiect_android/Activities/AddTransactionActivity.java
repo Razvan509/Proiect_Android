f659790f5537ffa56f12ccfc669036507c363e60
Razvan509 <razvanrang@gmail.com>
2020-05-01 12:59:55 +0300

    added activities

--- app/src/main/java/com/example/proiect_android/Activities/AddTransactionActivity.java
+++ app/src/main/java/com/example/proiect_android/Activities/AddTransactionActivity.java
@@ -1,26 +1,49 @@
-package com.example.proiect_android.Activities;
+package com.example.proiect_android.activities;
 
+import android.app.DatePickerDialog;
+import android.app.DatePickerDialog.OnDateSetListener;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.RadioGroup;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
 
-import com.example.proiect_android.Fragments.AddExpenseFragment;
-import com.example.proiect_android.Fragments.AddIncomeFragment;
-import com.example.proiect_android.Fragments.FirstPageFragment;
+import com.example.proiect_android.fragments.AddExpenseFragment;
+import com.example.proiect_android.fragments.AddIncomeFragment;
 import com.example.proiect_android.R;
 
+import java.util.Calendar;
+
 public class AddTransactionActivity extends AppCompatActivity {
+    public static final String EXTRA_DESCRIPTION =
+            "com.example.proiect_android.activities.EXTRA_DESCRIPTION";
+    public static final String EXTRA_AMOUNT =
+            "com.example.proiect_android.activities.EXTRA_AMOUNT";
+    public static final String EXTRA_CATEGORYID =
+            "com.example.proiect_android.activities.EXTRA_CATEGORYID";
+    public static final String EXTRA_DATE =
+            "com.example.proiect_android.activities.EXTRA_DATE";
+
 
     private FragmentManager fragmentManager;
     private FragmentTransaction fragmentTransaction;
 
-    private EditText editTextExpenseAmount;
+    private EditText editTextAmount;
+    private EditText editTextDescription;
+    private TextView textDate;
+    private RadioButton radioButtonNecessary;
+    private RadioButton radioButtonUnnecessary;
     private RadioGroup radioButtonSwitchBetweenTransactions;
+    private Button button;
     private RadioButton radioButtonExpenses;
     private RadioButton radioButtonIncome;
 
@@ -31,8 +54,10 @@ public class AddTransactionActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_transaction_main);
 
-        fragmentManager = getSupportFragmentManager();
 
+        fragmentManager = getSupportFragmentManager();
+//        getSupportActionBar().setHomeAsUpIndicator(R.drawable.ic_close);
+//        setTitle("Add transaction");
 
         radioButtonSwitchBetweenTransactions = (RadioGroup) findViewById(R.id.radio_group_switch_between_transactions_types);
         radioButtonExpenses = (RadioButton) findViewById(R.id.radio_button_expense);
@@ -58,6 +83,60 @@ public class AddTransactionActivity extends AppCompatActivity {
 
             }
         });
+
+    }
+
+    public void addTransactionButtonClicked(View view)
+    {
+        if(radioButtonExpenses.isChecked()) {
+            editTextAmount = findViewById(R.id.edit_text_expanse_amount);
+            editTextDescription = findViewById(R.id.edit_text_expanse_description);
+            textDate = findViewById(R.id.text_date_expense);
+        }
+
+        if(radioButtonIncome.isChecked()) {
+            editTextAmount = findViewById(R.id.edit_text_income_amount);
+            editTextDescription = findViewById(R.id.edit_text_income_description);
+            textDate = findViewById(R.id.text_date_income);
+        }
+
+        saveTransaction();
+    }
+
+    private void saveTransaction(){
+        String description = editTextDescription.getText().toString();
+        String amount = editTextAmount.getText().toString();
+        String transactionDate = textDate.getText().toString();
+        String categoryId = "";
+        if(radioButtonIncome.isChecked()){
+            categoryId = "1";
+        }else{
+            radioButtonNecessary = findViewById(R.id.radio_button_necessary);
+            radioButtonUnnecessary = findViewById(R.id.radio_button_unnecessary);
+            if (radioButtonNecessary.isChecked()) {
+                categoryId = "2";
+            }else{
+                if(radioButtonUnnecessary.isChecked()){
+                    categoryId = "3";
+                }else{
+                    Toast.makeText(this, "Please select the category of your transaction", Toast.LENGTH_SHORT).show();
+                    return;
+                }
+            }
+        }
+        if (description.trim().isEmpty() ||
+                amount.trim().isEmpty() || transactionDate.trim().isEmpty() || transactionDate.trim().isEmpty()){
+            Toast.makeText(this, "One or more fields are empty!", Toast.LENGTH_SHORT).show();
+            return;
+        }
+
+        Intent data = new Intent();
+        data.putExtra(EXTRA_DATE, transactionDate);
+        data.putExtra(EXTRA_CATEGORYID, categoryId);
+        data.putExtra(EXTRA_AMOUNT, amount);
+        data.putExtra(EXTRA_DESCRIPTION, description);
+        setResult(RESULT_OK, data);
+        finish();
     }
 
 
--- app/src/main/java/com/example/proiect_android/Activities/MainActivity.java
+++ app/src/main/java/com/example/proiect_android/Activities/MainActivity.java
@@ -1,16 +1,27 @@
-package com.example.proiect_android.Activities;
+package com.example.proiect_android.activities;
 
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
 import androidx.fragment.app.FragmentTransaction;
+import androidx.lifecycle.ViewModelProviders;
 
-import com.example.proiect_android.Fragments.AddIncomeFragment;
-import com.example.proiect_android.Fragments.FirstPageFragment;
+import com.example.proiect_android.beans.Transaction;
+import com.example.proiect_android.fragments.FirstPageFragment;
 import com.example.proiect_android.R;
 
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.example.proiect_android.activities.AddTransactionActivity;
+import com.example.proiect_android.models.TransactionViewModel;
+import com.example.proiect_android.models.UserViewModel;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -20,17 +31,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Intent data = getIntent();
+
+        FloatingActionButton buttonAddUser = findViewById(R.id.button_add_transaction);
 
         firstPageFragment = new FirstPageFragment();
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
         fragmentTransaction.add(R.id.container, firstPageFragment);
+        Bundle b = new Bundle();
+        b.putString("userId",data.getStringExtra(AuthActivity.EXTRA_USER));
+        firstPageFragment.setArguments(b);
         fragmentTransaction.commit();
+
+
     }
 
-    public void addTransactionClicked(View view)
-    {
-        Intent intent = new Intent(this, AddTransactionActivity.class);
-        startActivity(intent);
-    }
+
+
 }
